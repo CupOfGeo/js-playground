b3aeda72141d14802df2fb1c575ad2591e15f633
CupOfGeo <mazzeogeorge@gmail.com>
2023-01-22 21:40:31 -0600

    some inital js if syntax and conditionals

---
+++ hello-world.js
@@ -0,0 +1,15 @@
+// ifs and else if syntax 
+let shoppingDone = 999;
+let childsAllowance;
+
+if (shoppingDone === 10) {
+  childsAllowance = 10;
+} else if (shoppingDone < 10) {
+    childsAllowance = 9
+}
+else if (shoppingDone >= 13){
+ childsAllowance = 50
+}
+else {
+  childsAllowance = 5;
+}
